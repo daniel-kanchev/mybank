3d757bbf3df5e3977f53b30583bf25d219888286
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-08 13:17:19 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,13 @@
+URL: https://mybank.com/blog/
+
+    Spider name: mybank
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 12.98
+
+Number of articles: 50
---
+++ mybank/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'mybank'
+SPIDER_MODULES = ['mybank.spiders']
+NEWSPIDER_MODULE = 'mybank.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'mybank.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
